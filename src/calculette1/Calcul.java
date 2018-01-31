5f8b66b51c5241b22857a8396807de0fcca83346
Mizuho Toxe <mizuho.kimura@gmail.com>
2018-01-31 05:34:34 +0100

    calcul.java

--- src/calculette1/Calcul.java
+++ src/calculette1/Calcul.java
@@ -4,11 +4,37 @@ public class Calcul {
 
 	private int X;
 	private int Y;
+	private int add;
+	private int sous;
+	private int mult;
+	private int div;
 	
-	public Calcul(int X, int Y) {
+	public int add(int X, int Y) {
 		this.X = X;
 		this.Y = Y;
+		this.add = X+Y;
 	}
 	
+	public int sous(int X, int Y) {
+		this.X = X;
+		this.Y = Y;
+		this.sous = X-Y;
+	}
+	
+	public int mult(int X, int Y) {
+		this.X = X;
+		this.Y = Y;
+		this.mult = X*Y;
+	}
+	
+	public int div(int X, int Y) {
+		this.X = X;
+		this.Y = Y;
+		this.div = X/Y;
+	}
+	
+	public void Print() {
+		System.out.println("	addition : " + this.add);
+	}
 	
 }
